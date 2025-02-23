45ba1f7eb277ca2f1b87b70830c8a17e9d84639e
atiqur-rahman-0041 <atiqur.rahman0041@gmail.com>
2025-02-23 18:42:06 -0500

    Add geolocation functionality and improve weather API error handling

--- website/utils.py
+++ website/utils.py
@@ -1,4 +1,5 @@
 from google_images_search import GoogleImagesSearch
+import geocoder
 import requests
 import os
 
@@ -6,6 +7,17 @@ WEATHER_KEY = os.getenv("WEATHER_KEY")
 if not WEATHER_KEY:
     from projectsecrets.weather_secret import WEATHER_KEY
 
+class GeolocationAPI:
+    def __init__(self) -> None:
+        pass
+
+    """
+    Function to fetch current location using IP address
+    """
+
+    def getCurrentLocation(self):
+        g = geocoder.ip("me")
+        return g.city
 
 class WeatherAPI:
     def __init__(self) -> None:
@@ -17,17 +29,24 @@ class WeatherAPI:
 
     def getCurrentWeather(self, latitude=None, longitude=None, city=None):
         url = f"http://api.weatherapi.com/v1/current.json?key={WEATHER_KEY}&q={city}&aqi=no"
-        response = requests.request("GET", url, headers={}, data={})
-        if response.status_code != 200:
-            raise Exception(
-                "Weather API failed : response code : {code}".format(
-                    code=response.status_code
+
+        print("url: ", url, flush=True)
+
+        try:
+            response = requests.request("GET", url, headers={}, data={})
+            if response.status_code != 200:
+                raise Exception(
+                    "Weather API failed : response code : {code}".format(
+                        code=response.status_code
+                    )
                 )
-            )
-        jsonResponse = response.json()
-        if "condition" in jsonResponse:
-            return jsonResponse["condition"]["text"]
-        return ""
+            jsonResponse = response.json()
+            if "condition" in jsonResponse["current"]:
+                return jsonResponse["current"]["temp_f"], jsonResponse["current"]["condition"]["text"]
+            return (None, None)
+        except Exception as e:
+            print("Error fetching weather: ", str(e), flush=True)
+            return (None, None)
 
     """
     Function to fetch weather forecast for future from external weather API
@@ -61,36 +80,18 @@ class ImageConfig:
         self.API_KEY = os.environ.get("GOOGLE_IMAGES_API_KEY")
         self.PROJ_CX = os.environ.get("GOOGLE_IMAGES_PROJ_CX")
 
-
-class QueryBuilder:
-    def __init__(self) -> None:
-        pass
-
-    def getQueryString(self, queries, culture=""):
-        return_query_string = ""
-        for q in queries:
-            return_query_string += q + " "
-
-        return "Suggest " + culture + " outfits for " + return_query_string
-
-
 class SearchImages:
     def __init__(self) -> None:
         self.config = ImageConfig()
         self.gis = GoogleImagesSearch(self.config.API_KEY, self.config.PROJ_CX)
         self.default_num_of_records = 10
-        self.query_builder = QueryBuilder()
 
     # gives the list of urls for a search
-    def image_search(self, query_keywords, culture=None, num_of_records=None):
+    def image_search(self, query, num_of_records=None):
         if not num_of_records:
             num_of_records = self.default_num_of_records
         
-        if not culture:
-            query = query_keywords
-        else:
-            query = self.query_builder.getQueryString(query_keywords, culture=culture)
-        print("Searchingy ", query)
+        print("Searching: ", query, flush=True)
         _search_params = {"q": query, "num": num_of_records}
         self.gis.search(search_params=_search_params)
 
